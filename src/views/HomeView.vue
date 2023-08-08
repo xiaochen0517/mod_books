b2b32b485728f7f5593f76fda3bf678c8e182ceb
mochenya <2827075398@qq.com>
2023-08-08 20:49:00 +0800

    config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,22 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import axios from "@/plugins/axios";
+import yaml from "js-yaml";
+import defaultSetting from "@/assets/json/default_setting.json";
+
+const configSetting = ref<any>(null);
 
 onMounted(() => {
   // 使用axios获取根路径下的 mod_setting.yml 文件并解析
   axios
     .get("/mod_setting.yml")
     .then((res) => {
-      console.log(res.data);
-      
+      // 使用js-yaml解析
+      const data: any = yaml.load(res.data);
+      configSetting.value = { ...defaultSetting, ...data };
+      console.log("configSetting", configSetting.value);
     })
     .catch((err) => {
       console.log(err);
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: './src/docs',
+            to: './',
+            noErrorOnMissing: true,
+          },
+        ],
+      }),
+    ],
+  },
 })
