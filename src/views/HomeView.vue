515677b01cb76e20489d4dec5dfda24367903a62
mochenya <2827075398@qq.com>
2023-08-09 09:45:06 +0800

    pages config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="home-view"></div>
+  <div class="home-view">
+    <div class="catalogue-list">
+      <div
+        class="catalogue-item"
+        v-for="(item, index) in configPages.pages"
+        :key="index"
+      >
+        {{ item.name }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,19 +19,25 @@ import yaml from "js-yaml";
 import defaultSetting from "@/assets/json/default_setting.json";
 
 const configSetting = ref<any>(null);
+const configPages = ref<any>({ pages: [] });
 
-onMounted(() => {
-  // 使用axios获取根路径下的 mod_setting.yml 文件并解析
-  axios
-    .get("/mod_setting.yml")
-    .then((res) => {
-      // 使用js-yaml解析
-      const data: any = yaml.load(res.data);
-      configSetting.value = { ...defaultSetting, ...data };
-      console.log("configSetting", configSetting.value);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+onMounted(async () => {
+  await getModSetting();
+  await getPagesConfig();
+  // test
+  console.log(configSetting.value);
+  console.log(configPages.value);
 });
+
+const getModSetting = async () => {
+  let res = await axios.get("/mod_setting.yml");
+  let settingConfig: any = yaml.load(res.data);
+  configSetting.value = { ...defaultSetting, ...settingConfig };
+};
+
+const getPagesConfig = async () => {
+  let res = await axios.get("/mod_pages.yml");
+  let pagesConfig: any = yaml.load(res.data);
+  configPages.value = pagesConfig;
+};
 </script>
