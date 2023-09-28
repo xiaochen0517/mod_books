09eba84276de6985888e354c1db44abd6e2e454e
mochenya <2827075398@qq.com>
2023-09-28 14:48:58 +0800

    Theme本地储存功能

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,12 @@ const routes = [
     name: "about",
     component: () => import("../views/AboutView.vue"),
   },
+  // 跳转外部链接页面
+  {
+    path: "/external/:encodeUrl",
+    name: "external",
+    component: () => import("../views/ExternalView.vue"),
+  }
 ];
 
 const router = createRouter({
